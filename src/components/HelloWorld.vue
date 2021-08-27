658a04537a9bbb804f9816b8e9e063dd0e26b623
alpersad <cybernorp@gmail.com>
2021-08-27 19:49:32 -0400

    next holiday card and logic added

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,22 @@
 <template>
-  <div>
-    <pre>{{ dates }}</pre>
+  <div class="d-flex justify-center">
+    <!-- <pre>{{ dates[index] }}</pre> -->
+    <v-card class="mt-16" max-width="600">
+      <v-card-title>
+        <p>
+          <span class="text-md-subtitle-1 text-body-2 font-weight-light"
+            >Next Holiday</span
+          >
+          <br />
+          <span class="text-md-h3 text-h5">{{ day }} </span>
+        </p>
+      </v-card-title>
+      <v-card-subtitle
+        ><span class="text-subtitle-1 font-weight-bold"
+          >{{ name }}
+        </span></v-card-subtitle
+      >
+    </v-card>
   </div>
 </template>
 
@@ -12,13 +28,14 @@ export default {
   data() {
     return {
       dates: [],
-      dates_index: 0,
-      holidays: Holidays,
+      index: 0,
+
+      holidays: Holidays
     };
   },
-  mounted() {
+  beforeMount() {
     this.dates = this.parseHolidays();
-    this.dates_index = this.nextHoliday();
+    this.index = this.nextHoliday();
   },
   methods: {
     nextHoliday() {
@@ -30,24 +47,52 @@ export default {
     },
     parseHolidays() {
       const {
-        response: { holidays },
+        response: { holidays }
       } = this.holidays;
       let dates = [];
       for (const holiday of holidays) {
         let { name } = holiday;
         let {
-          date: { iso },
+          date: { iso }
         } = holiday;
         let date = {
           name: name,
           date: dayjs(iso),
-          weight: dayjs(iso).diff(dayjs(), "d"),
+          weight: Math.ceil(dayjs(iso).diff(dayjs(), "d", true))
         };
         dates.push(date);
       }
       return dates;
     },
+    getDayDisplay() {
+      let weekday = this.dates[this.index].date.format("dddd");
+      let day_of_month = this.dates[this.index].date.format("D");
+      let month = this.dates[this.index].date.format("MMMM");
+      let year = this.dates[this.index].date.format("YYYY");
+      console.log(day_of_month[1]); // Array position 1 stores the lowest significant digit of the day of the month
+      let order =
+        day_of_month[1] == 1
+          ? "st"
+          : day_of_month[1] == 2
+          ? "nd"
+          : day_of_month[1] == 3
+          ? "rd"
+          : "th";
+      return `${weekday} ${day_of_month}${order} ${month} ${year}`;
+    },
+    getNextHolidayName() {
+      return this.dates[this.index].name;
+    }
   },
+
+  computed: {
+    day: function() {
+      return this.getDayDisplay();
+    },
+    name: function() {
+      return this.getNextHolidayName();
+    }
+  }
 };
 </script>
 
